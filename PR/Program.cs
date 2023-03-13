8e36698c823970f9be10ebafce7267b174de454d
Бельский Артём Михайлович <320102@college.local>
2023-03-13 09:06:29 +0300

    first commit

---
+++ PR/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PR
+{
+    class Program
+    {
+        public class River
+        {
+            string Name { get; set; }
+            double Squarepool { get; set; }
+            double Length { get; set; }
+
+            public River(string name, double squarepool, double lenght)
+            {
+                Name = name;
+                Squarepool = squarepool;
+                Length = lenght;
+            }
+
+
+        }
+       
+    }
+}
